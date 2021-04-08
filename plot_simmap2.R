ba32557a810f62b1ec94308e71b4ef0a35350a24
Hao Chi Kiang <hello@hckiang.com>
2021-04-08 13:25:34 +0200

    Tree plots

--- plot_simmap2.R
+++ plot_simmap2.R
@@ -5,45 +5,41 @@ suppressPackageStartupMessages({
 })
 
 
-mods = c("FULL", "LAMBSAME", "QPAIR", "LAMBSAMEQPAIR", "QEQ", "ALLEQ")
+## Change these to plot different models
+character_file = "chain_data/RES_HISSE_ST6NOULTRA_QPAIR_WITHMU_2/marginal_character01.tree"
+outfile        = "plots/qpair-withmu-simmap-fan-01.pdf"
 
-for (m in mods) {
-  respath = function (x) sprintf("RES_HISSE_ST6NOULTRA_%s_1/%s", m, x)
-  character_file = respath("marginal_character.tree")
-  astree_file = respath("ast.tree")
-  write_pdf = TRUE
+## Whether to plot in new window or plot in PDF
+write_pdf      = T
 
-  if (write_pdf) pdf(respath("simmap_fan.pdf"), width = 50, height = 50)
+if (write_pdf) pdf(outfile, width = 50, height = 50)
 
-  sim2 = read.simmap(file=character_file, format="phylip")
+sim2 = read.simmap(file=character_file, format="phylip")
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+colv = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+## pie(rep(1,n), col=col_vector)
+colsel = colv[c(48, 18, 52, 22, 26, 16,
+                9 , 17, 15, 56, 25, 54)]
+colsel = setNames(colsel, as.character(0:11))
+plotSimmap(sim2, colsel, type = "fan", ftype="off", lwd=3, offset=1, split.vertical = T)
 
-  n = 60
-  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-  colv = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-  ## pie(rep(1,n), col=col_vector)
-  colsel = colv[c(48, 18, 52, 22, 26, 16,
-                  9 , 17, 15, 56, 25, 54)]
-  colsel = setNames(colsel, as.character(0:11))
-  plotSimmap(sim2, colsel, type = "fan", ftype="off", lwd=3, offset=1, split.vertical = T)
+x = 3.52
+y = 4.80
 
-  x = 3.52
-  y = 4.80
-
-  trans_ord = function (x) {
-    dim(x) = c(length(x)/2,2)
-    x = t(x)
-    c(x)
-  }
-
-  labs = c("Africa A", "Asia A", "Europe A", "N.Amer. A", "Oceania A", "S.Amer. A",
-           "Africa B", "Asia B", "Europe B", "N.Amer. B", "Oceania B", "S.Amer. B")
-  labst = trans_ord(labs)
-  y = y - (0:(length(colsel) - 1)) * 0.25
-  x = rep(x, length(y))
-  text(x + 0.2, y, labst, pos=4, cex=7.4, font=2)
-  colselt = trans_ord(colsel)
-  mapply(draw.circle, x=x, y=y, col=colselt, MoreArgs = list(nv=200, radius=0.08, border="white"))
-
-  if (write_pdf)  dev.off()
-
+trans_ord = function (x) {
+  dim(x) = c(length(x)/2,2)
+  x = t(x)
+  c(x)
 }
+
+labs = c("Africa A", "Asia A", "Europe A", "N.Amer. A", "Oceania A", "S.Amer. A",
+         "Africa B", "Asia B", "Europe B", "N.Amer. B", "Oceania B", "S.Amer. B")
+labst = trans_ord(labs)
+y = y - (0:(length(colsel) - 1)) * 0.25
+x = rep(x, length(y))
+text(x + 0.2, y, labst, pos=4, cex=7.4, font=2)
+colselt = trans_ord(colsel)
+mapply(draw.circle, x=x, y=y, col=colselt, MoreArgs = list(nv=200, radius=0.08, border="white"))
+
+if (write_pdf)  dev.off()
+
--- plot_simmap3.R
+++ plot_simmap3.R
@@ -4,43 +4,38 @@ suppressPackageStartupMessages({
   library(RColorBrewer)
 })
 
+## Change these to plot different models
+character_file = "chain_data/RES_HISSE_ST6NOULTRA_QPAIR_WITHMU_2/marginal_character01.tree"
+outfile        = "plots/qpair-withmu-simmap-regionfan-01.pdf"
 
-mods = c("FULL", "LAMBSAME", "QPAIR", "LAMBSAMEQPAIR", "QEQ", "ALLEQ")
+## Whether to plot in new window or plot in PDF
+write_pdf      = T
 
-for (m in mods) {
-  respath = function (x) sprintf("RES_HISSE_ST6NOULTRA_%s_1/%s", m, x)
-  character_file = respath("marginal_character.tree")
-  astree_file = respath("ast.tree")
-  write_pdf = TRUE
+if (write_pdf) pdf(outfile, width = 50, height = 50)
+sim2 = read.simmap(file=character_file, format="phylip")
 
-  if (write_pdf) pdf(respath("simmap_regionfan.pdf"), width = 50, height = 50)
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+colv = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+## pie(rep(1,n), col=colv)
+colsel = colv[rep(c(24, 18, 46, 48, 60, 16),2)]
+colsel = setNames(colsel, as.character(0:11))
+plotSimmap(sim2, colsel, type = "fan", ftype="off", lwd=3, offset=1, split.vertical = T)
+##  plotSimmap(sim2, colsel, type = "fan", fsize=0.7, lwd=3, offset = 1, split.vertical = T)
 
-  sim2 = read.simmap(file=character_file, format="phylip")
+x = 2.85
+y = 4.7
 
-  n = 60
-  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-  colv = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-  ## pie(rep(1,n), col=colv)
-  colsel = colv[rep(c(24, 18, 46, 48, 60, 16),2)]
-  colsel = setNames(colsel, as.character(0:11))
-  plotSimmap(sim2, colsel, type = "fan", ftype="off", lwd=3, offset=1, split.vertical = T)
-  ##  plotSimmap(sim2, colsel, type = "fan", fsize=0.7, lwd=3, offset = 1, split.vertical = T)
-
-  x = 2.85
-  y = 4.7
-
-  trans_ord = function (x) {
-    dim(x) = c(length(x)/2,2)
-    x = t(x)
-    c(x)
-  }
-
-  labs = c("Africa", "Asia", "Europe", "N. America", "Oceania", "S. America")
-  y = y - (0:(length(colsel)/2 - 1)) * 0.35
-  x = rep(x, length(y))
-  text(x + 0.2, y, labs, pos=4, cex=10.3, font=2)
-  colselt = colsel[1:6]
-  mapply(draw.circle, x=x, y=y, col=colselt, MoreArgs = list(nv=200, radius=0.08, border="white"))
-
-  if (write_pdf)  dev.off()
+trans_ord = function (x) {
+  dim(x) = c(length(x)/2,2)
+  x = t(x)
+  c(x)
 }
+
+labs = c("Africa", "Asia", "Europe", "N. America", "Oceania", "S. America")
+y = y - (0:(length(colsel)/2 - 1)) * 0.35
+x = rep(x, length(y))
+text(x + 0.2, y, labs, pos=4, cex=10.3, font=2)
+colselt = colsel[1:6]
+mapply(draw.circle, x=x, y=y, col=colselt, MoreArgs = list(nv=200, radius=0.08, border="white"))
+
+if (write_pdf)  dev.off()
